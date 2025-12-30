fe67aa2717812dbe6f76b4a2cb080fea89419ef4
wty2019wty <wty2019wty@163.com>
2025-12-30 15:09:54 +0800

    cf 反代加速

--- base/src/main/java/com/rtbishop/look4sat/domain/data/IRemoteDataSource.kt
+++ base/src/main/java/com/rtbishop/look4sat/domain/data/IRemoteDataSource.kt
@@ -21,7 +21,7 @@ import java.io.InputStream
 
 interface IRemoteDataSource {
 
-    val radioApi: String get() = "https://db.satnogs.org/api/transmitters/?format=json&status=active"
+    val radioApi: String get() = "https://tle.486520.xyz/https://db.satnogs.org/api/transmitters/?format=json&status=active"
 
     suspend fun getDataStream(url: String): InputStream?
 }
